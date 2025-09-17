6527b0f399641cdff41521adedc0fd44bee3a86c
Glenda Poblete <glenfamydigo@gmail.com>
2025-09-17 17:30:38 +0800

    commented the weather.js codes

--- weather-app/src/weather.js
+++ weather-app/src/weather.js
@@ -1,13 +1,13 @@
-export const fetchWeather = async (city) => {
-  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// export const fetchWeather = async (city) => {
+//   const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  const response = await fetch(url);
+//   const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error("City not found");
-  }
+//   if (!response.ok) {
+//     throw new Error("City not found");
+//   }
 
-  const data = await response.json();
-  return data;
-};
+//   const data = await response.json();
+//   return data;
+// };
